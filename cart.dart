3bd04074abd0b1bc62f9d4f94b04567fd79844a4
a7medjamal <ahmedjmal20003@gmail.com>
2025-03-12 01:37:37 +0200

    add: cart class

---
+++ cart.dart
@@ -0,0 +1,33 @@
+import 'product.dart';
+
+class Cart {
+  List<Product> products = [];
+
+  void addToCart(Product product) {
+    products.add(product);
+    print('${product.name} has been added to your cart.');
+  }
+
+  double calculateTotalPrice() {
+    double total = 0.0;
+    for (var product in products) {
+      total += product.price;
+    }
+    return total;
+  }
+
+  void viewCart() {
+    if (products.isEmpty) {
+      print('Your cart is empty.');
+    }
+    print('Your Cart:\n');
+    for (var product in products) {
+      product.showProductDetails();
+    }
+  }
+
+  void clearCart() {
+    products.clear();
+    print('Your cart has been cleared.');
+  }
+}
